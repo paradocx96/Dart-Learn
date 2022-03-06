0cdb89ee2b21c802eec3279d1ea48590d58b2aaf
Navinda Lankesh <adilanga@gmail.com>
2022-03-06 15:54:15 +0530

    Environment Updated

---
+++ Environment/bin/variables.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  var name = 'Voyager I';
+  print(name);
+
+  var year = 1977;
+  print(year);
+
+  var antennaDiameter = 3.7;
+  print(antennaDiameter);
+
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  print(flybyObjects);
+
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+  print(image);
+}

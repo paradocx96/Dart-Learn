c5097faea4b1ebbe75874665289640501df9ea9d
Navinda Lankesh <adilanga@gmail.com>
2022-03-06 17:50:34 +0530

    Dart - Closures

---
+++ Closure/closure.dart
@@ -0,0 +1,13 @@
+typedef AddFunction = int Function(int);
+
+AddFunction makeAdder(int addBy) {
+return (int i) => addBy + i;
+}
+
+void main() {
+  AddFunction add2 = makeAdder(2);
+  AddFunction add4 = makeAdder(4);
+
+  print(add2(3) == 5);
+  print(add4(3) == 7);
+}
